67aff8fe32b0b1ec73782c2006cff5136bcea754
melo0115 <melo0115@yahoo.com>
2021-12-18 20:55:34 -0600

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,38 @@
+name = 'Joshua K. Buggs'
+age = 33 # not a lie
+height = 74 # inches
+weight = 265 # lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+
+print (f"Let's talk about {name}.")
+print (f"He's {height} inches tall.")
+print (f"He's {weight} pounds heavy.")
+print ("Actually that's not too heavy.")
+print (f"He's got {eyes} eyes and {hair} hair.")
+print (f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print (f"I add {age}, {height}, and {weight} I get {total}.")
+
+length_inch = 1
+length_centimeter = 2.54 
+
+weight_pounds = 1
+weight_kilograms = 0.453592
+
+length_inch += 10 
+print (length_inch * length_centimeter)
+
+print (f"I have a board this is 20 inches. How much is that in centimeters? {20 * length_centimeter}")
+
+print (f"The elephant weights 2000 kilograms. What does that translate to in pounds? {2000 * weight_kilograms}")
+
+tyd23 = "zong mayer"
+
+print (tyd23)
+
+round (3.5892)
+
